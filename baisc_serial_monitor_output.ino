8959a01e1667e6f962b56c84f682bd55927774a5
vaibhav0012 <39331749+vaibhav0012@users.noreply.github.com>
2019-10-13 12:01:13 +0530

    Add files via upload

---
+++ baisc_serial_monitor_output.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for (int i=0;i<100;i++)
+{
+  Serial.println(i);
+}
+}
